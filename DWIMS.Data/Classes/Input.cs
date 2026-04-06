e15cec520e64d81dec992f0f890982cc3e4dd7ac
Adrian Javier <agjavier@kld.edu.ph>
2026-04-07 00:31:44 +0800

    Add supporting info to Input.cs

--- DWIMS.Data/Classes/Input.cs
+++ DWIMS.Data/Classes/Input.cs
@@ -3,6 +3,8 @@ namespace DWIMS.Data;
 public class Input
 {
     public int Id { get; set; }
+    public string Title { get; set; }
     public string value { get; set; }
     public InputTypes Type { get; set; }
+    public bool Required { get; set; } = true;
 }
