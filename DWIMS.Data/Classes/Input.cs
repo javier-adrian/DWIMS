c6cf776dbbc34aec81bfc4f7f2156c2840d7585b
Adrian Javier <agjavier@kld.edu.ph>
2026-04-07 02:27:27 +0800

    Fix casing in Input.cs

--- DWIMS.Data/Classes/Input.cs
+++ DWIMS.Data/Classes/Input.cs
@@ -6,7 +6,7 @@ public class Input : ISoftDeletable
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string value { get; set; }
+    public string Value { get; set; }
     public InputTypes Type { get; set; }
     public bool Required { get; set; } = true;
     
