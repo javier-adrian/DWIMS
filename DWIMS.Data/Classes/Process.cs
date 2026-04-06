72adc860ba8fae11d2a4abc30a284a53379f01b6
Adrian Javier <agjavier@kld.edu.ph>
2026-04-07 00:28:24 +0800

    Add title to Process.cs

--- DWIMS.Data/Classes/Process.cs
+++ DWIMS.Data/Classes/Process.cs
@@ -3,6 +3,7 @@ namespace DWIMS.Data;
 public class Process
 {
     public int Id { get; set; }
+    public string Title { get; set; }
     public ICollection<Step> Steps { get; set; }
     public Document Document { get; set; }
 }
