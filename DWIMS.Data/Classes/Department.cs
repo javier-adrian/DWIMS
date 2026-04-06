3eac8c05b9c8909a1778ea42a19d866330332828
Adrian Javier <agjavier@kld.edu.ph>
2026-04-07 00:25:11 +0800

    Add department title

--- DWIMS.Data/Classes/Department.cs
+++ DWIMS.Data/Classes/Department.cs
@@ -3,6 +3,7 @@ namespace DWIMS.Data;
 public class Department
 {
     public int Id { get; set; }
+    public string Title { get; set; }
     public ICollection<Document> Documents { get; set; }
     public ICollection<User> Users { get; set; }
 }
