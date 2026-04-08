be0f011351422aaae6e9cb80f91f7df68c86ff30
Adrian Javier <agjavier@kld.edu.ph>
2026-04-09 05:06:44 +0800

    add swagger support

--- DWIMS.App/Program.cs
+++ DWIMS.App/Program.cs
@@ -1,11 +1,24 @@
 namespace DWIMS;
 
+using Scalar.AspNetCore;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen(
+                x => 
+                    x.SwaggerDoc("v1", new() { Title = "DWIMS API", Version = "v1" })
+                );
+        
         var app = builder.Build();
+        
+        app.MapSwagger("/openapi/{documentName}.json");
+        app.MapScalarApiReference();
 
         app.MapGet("/", () => "Hello World!");
 
