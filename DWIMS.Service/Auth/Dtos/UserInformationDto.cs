42bc6f7c83c8b5dca9fdaaae8e3df59b5a04a6f0
javier-adrian <agjavier@kld.edu.ph>
2026-04-13 03:43:35 +0800

    Add DTO for user information

---
+++ DWIMS.Service/Auth/Dtos/UserInformationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DWIMS.Service.Auth.Dtos;
+
+public sealed record UserInformationDto(
+    Guid Id,
+    string FullName,
+    string Email,
+    string? ContactNumber,
+    bool hasSignature);
