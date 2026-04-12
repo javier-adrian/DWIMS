4337629582fd4db2278aa9c1e14d93a19510fb58
javier-adrian <agjavier@kld.edu.ph>
2026-04-13 03:39:38 +0800

    Add service interface for tokens

---
+++ DWIMS.Service/Auth/ITokenService.cs
@@ -0,0 +1,23 @@
+﻿using DWIMS.Data;
+
+namespace DWIMS.Service.Auth;
+
+public interface ITokenService
+{
+    string GenerateAccessToken(
+        User user,
+        IEnumerable<Role> roles);
+    
+    Task<string> GenerateRefreshTokenAsync(
+        Guid userId,
+        CancellationToken cancellationToken = default);
+    
+    Task<string> ValidateRefreshTokenAsync(
+        Guid userId,
+        string refreshToken,
+        CancellationToken cancellationToken = default);
+
+    Task RevokeRefreshTokenAsync(
+        Guid userId,
+        CancellationToken cancellationToken = default);
+}
