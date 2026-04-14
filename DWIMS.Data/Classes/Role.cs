7dba3b3633fa7c03e6e90fae779d2da7d0a654ce
javier-adrian <agjavier@kld.edu.ph>
2026-04-15 06:49:17 +0800

    Add department and user to specific roles

--- DWIMS.Data/Classes/Role.cs
+++ DWIMS.Data/Classes/Role.cs
@@ -7,7 +7,10 @@ public class Role : ISoftDeletable
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public Department Department { get; set; }
     public ICollection<Document> Documents { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
     
     public bool IsDeleted { get; set; }
     public DateTime? DeletedOn { get; set; }
