28ca26b2339752ec376d19c308d6bb87bc78822a
javier-adrian <agjavier@kld.edu.ph>
2026-04-16 05:57:36 +0800

    create extension methods for Results

---
+++ DWIMS.Service/Services/ResultExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DWIMS.Service.Common;
+using Microsoft.AspNetCore.Http;
+
+namespace DWIMS.Service.Services;
+
+public static class ResultExtensions
+{
+    public static IResult ToCreatedResult<T>(
+        this Result<T> result,
+        string location) =>
+        result.IsSuccess
+            ? Results.Created(location, result.Data)
+            : Results.UnprocessableEntity(new
+            {
+                result.Error,
+                result.ErrorDescription
+            });
+}
