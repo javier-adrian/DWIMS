9bb552a39504000754f82753ea91c0eac268e397
javier-adrian <agjavier@kld.edu.ph>
2026-04-16 11:06:26 +0800

    add user endpoints

--- DWIMS.App/Endpoints/UserEndpoints.cs
+++ DWIMS.App/Endpoints/UserEndpoints.cs
@@ -2,5 +2,39 @@ namespace DWIMS.Controllers;
 
 public static class UserEndpoints
 {
-    
+    public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("users")
+            .WithTags("Users")
+            .RequireAuthorization();
+        
+        group.MapGet("/me", GetCurrentuser)
+            .WithDisplayName("Get Current User")
+            .WithSummary("Get the current user's profile");
+        
+        group.MapPut("/me", UpdateCurrentuser)
+            .WithDisplayName("Update Current User")
+            .WithSummary("Update the current user's profile");
+        
+        group.MapPut("/me/signature", UploadSignature)
+            .WithDisplayName("Upload Signature")
+            .WithSummary("Upload a signature for the current user");
+        
+        return app;
+    }
+
+    private static Task UploadSignature(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Task UpdateCurrentuser(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Task GetCurrentuser(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
 }
