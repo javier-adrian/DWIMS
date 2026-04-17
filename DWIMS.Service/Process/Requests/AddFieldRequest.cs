a5c107eea3ecf154928409f2b286da9906e2e4af
javier-adrian <agjavier@kld.edu.ph>
2026-04-18 06:01:49 +0800

    fix data type

---
+++ DWIMS.Service/Process/Requests/AddFieldRequest.cs
@@ -0,0 +1,11 @@
+﻿using DWIMS.Data;
+
+namespace DWIMS.Service.Process.Requests;
+
+public sealed class AddFieldRequest
+{
+    public required Guid ProcessId { get; set; }
+    public required string Title { get; set; }
+    public required InputTypes InputType { get; set; }
+    public bool Required { get; set; } = true;
+}
