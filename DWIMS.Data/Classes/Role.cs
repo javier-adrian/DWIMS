d2483a19de60da52b93d050b5219007a73290855
javier-adrian <agjavier@kld.edu.ph>
2026-04-17 23:19:07 +0800

    make departments nullable to roles

--- DWIMS.Data/Classes/Role.cs
+++ DWIMS.Data/Classes/Role.cs
@@ -8,8 +8,8 @@ public class Role : ISoftDeletable
     public string Title { get; set; }
     public string Description { get; set; }
     public GeneralRole GeneralRole { get; set; }
-    public Guid DepartmentId { get; set; }
-    public Department Department { get; set; }
+    public Guid? DepartmentId { get; set; }
+    public Department? Department { get; set; }
     public ICollection<Document> Documents { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; }
