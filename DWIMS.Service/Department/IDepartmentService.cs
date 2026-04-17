d8b6d76134545404ac0a5f066f6fa333ad368057
javier-adrian <agjavier@kld.edu.ph>
2026-04-17 22:13:04 +0800

    add interface for department service

---
+++ DWIMS.Service/Department/IDepartmentService.cs
@@ -0,0 +1,29 @@
+﻿using DWIMS.Service.Common;
+using DWIMS.Service.Department.Dtos;
+using DWIMS.Service.Department.Requests;
+
+namespace DWIMS.Service.Department;
+
+public interface IDepartmentService
+{
+    Task<Result<IReadOnlyList<DepartmentDto>>> GetDepartmentAsync(
+        CancellationToken cancellationToken = default);
+
+    Task<Result<Guid>> CreateDepartmentAsync(
+        CreateDepartmentRequest request,
+        CancellationToken cancellationToken = default);
+
+    Task<Result> UpdateDepartmentAsync(
+        Guid id,
+        UpdateDepartmentRequest request,
+        CancellationToken cancellationToken = default);
+
+    Task<Result> DeleteDepartmentAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+    
+    Task<Result> AssignRoleAsync(
+        Guid departmentId, 
+        AssignRoleRequest request,
+        CancellationToken cancellationToken = default);
+}
