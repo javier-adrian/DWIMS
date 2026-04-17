695eddf112654912e9cf50e623ae38c59c6ef584
javier-adrian <agjavier@kld.edu.ph>
2026-04-18 05:55:30 +0800

    add direct id column to field entity

---
+++ DWIMS.Data/Migrations/20260417215835_AddDirectIdColumns.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DWIMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDirectIdColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Departments_DepartmentId",
+                table: "Steps");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Field_FieldId",
+                table: "Steps");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Processes_ProcessId",
+                table: "Steps");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProcessId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FieldId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Departments_DepartmentId",
+                table: "Steps",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Field_FieldId",
+                table: "Steps",
+                column: "FieldId",
+                principalTable: "Field",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Processes_ProcessId",
+                table: "Steps",
+                column: "ProcessId",
+                principalTable: "Processes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Departments_DepartmentId",
+                table: "Steps");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Field_FieldId",
+                table: "Steps");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Steps_Processes_ProcessId",
+                table: "Steps");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProcessId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "FieldId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Steps",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Departments_DepartmentId",
+                table: "Steps",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Field_FieldId",
+                table: "Steps",
+                column: "FieldId",
+                principalTable: "Field",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Steps_Processes_ProcessId",
+                table: "Steps",
+                column: "ProcessId",
+                principalTable: "Processes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
