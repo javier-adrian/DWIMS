11a16de02a7baa6ae212a81387e01631ca1e038f
javier-adrian <agjavier@kld.edu.ph>
2026-04-17 17:47:22 +0800

    add request models for Users

---
+++ DWIMS.Service/CurrentUser/Dtos/UserProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DWIMS.Service.CurrentUser.Dtos;
+
+public sealed record UserProfileDto(
+    Guid Id,
+    string FirstName,
+    string? MiddleName,
+    string LastName,
+    string Email,
+    string? ContactNumber, 
+    bool hasSignature);
---
+++ DWIMS.Service/CurrentUser/Requests/UpdateProfileRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace DWIMS.Service.CurrentUser.Requests;
+
+public class UpdateProfileRequest
+{
+    public required string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public required string LastName { get; set; }
+    
+    public required string Email { get; set; }
+    public string? ContactNumber { get; set; }
+    public DateTime? BirthDate { get; set; }
+}
