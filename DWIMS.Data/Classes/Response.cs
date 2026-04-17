d731b07ec09c78bf0ab9f3912f88a8c2000f3145
javier-adrian <agjavier@kld.edu.ph>
2026-04-18 07:59:54 +0800

    add direct id columns

--- DWIMS.Data/Classes/Response.cs
+++ DWIMS.Data/Classes/Response.cs
@@ -5,8 +5,11 @@ namespace DWIMS.Data;
 public class Response : ISoftDeletable
 {
     public Guid Id { get; set; }
+    public Guid SubmissionId { get; set; }
     public Submission Submission { get; set; }
+    public Guid StepId { get; set; }
     public Step Step { get; set; }
+    public Guid? ReviewerId { get; set; }
     public User Reviewer { get; set; }
     public Status Result { get; set; }
     public string Remarks { get; set; }
