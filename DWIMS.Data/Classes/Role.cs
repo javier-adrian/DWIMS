152f71ed4304c89736c32c67affc3b7b5307fae2
javier-adrian <agjavier@kld.edu.ph>
2026-04-17 23:10:54 +0800

    add direct department id to role entity

--- DWIMS.Data/Classes/Role.cs
+++ DWIMS.Data/Classes/Role.cs
@@ -8,6 +8,7 @@ public class Role : ISoftDeletable
     public string Title { get; set; }
     public string Description { get; set; }
     public GeneralRole GeneralRole { get; set; }
+    public Guid DepartmentId { get; set; }
     public Department Department { get; set; }
     public ICollection<Document> Documents { get; set; }
     public Guid UserId { get; set; }
