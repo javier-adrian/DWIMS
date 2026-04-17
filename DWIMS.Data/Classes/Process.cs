80000b0302c7c2d1b80ca5df5a5dc6311d8f8554
javier-adrian <agjavier@kld.edu.ph>
2026-04-18 03:22:40 +0800

    add department to process entity

--- DWIMS.Data/Classes/Process.cs
+++ DWIMS.Data/Classes/Process.cs
@@ -5,10 +5,12 @@ namespace DWIMS.Data;
 public class Process : ISoftDeletable
 {
     public Guid Id { get; set; }
+    public Guid DepartmentId { get; set; }
     public string Title { get; set; }
     public ICollection<Step> Steps { get; set; }
     public ICollection<Field> Fields { get; set; }
     public Document Document { get; set; }
+    public Department Department { get; set; }
     
     public bool IsDeleted { get; set; }
     public DateTime? DeletedOn { get; set; }
---
+++ DWIMS.Data/Migrations/20260417192148_AddDepartmentToProcesses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DWIMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentToProcesses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "Processes",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Processes_DepartmentId",
+                table: "Processes",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Processes_Departments_DepartmentId",
+                table: "Processes",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Processes_Departments_DepartmentId",
+                table: "Processes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Processes_DepartmentId",
+                table: "Processes");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Processes");
+        }
+    }
+}
