236dfc3287098e47415f68ed4216769091fb1068
javier-adrian <agjavier@kld.edu.ph>
2026-04-18 16:56:31 +0800

    add password reset tokens

---
+++ DWIMS.Data/Classes/PasswordResetToken.cs
@@ -0,0 +1,12 @@
+﻿namespace DWIMS.Data;
+
+public class PasswordResetToken
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public required string Token { get; set; }
+    public DateTime Expires { get; set; }
+    public bool Used { get; set; }
+    public DateTime Created { get; set; }
+    public User User { get; set; }
+}
---
+++ DWIMS.Data/Migrations/20260418085606_AddPasswordResetTokens.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DWIMS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
