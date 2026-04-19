27a5efbcae8447fc4d813df57118895e71c2b47d
javier-adrian <agjavier@kld.edu.ph>
2026-04-19 22:18:09 +0800

    add dto for uploading document templates

---
+++ DWIMS.Service/Process/Requests/UploadDocumentTemplateRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DWIMS.Service.Process.Requests;
+
+public sealed class UploadDocumentTemplateRequest
+{
+    public required Stream File { get; init; }
+    public required string FileName { get; init; }
+    public required IReadOnlyList<Guid> Fields { get; init; }
+}
