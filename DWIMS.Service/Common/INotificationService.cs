65c87eaba122d0c186ac635ca504d33d568410f0
javier-adrian <agjavier@kld.edu.ph>
2026-04-20 04:25:14 +0800

    add interface for notification

---
+++ DWIMS.Service/Common/INotificationService.cs
@@ -0,0 +1,21 @@
+﻿namespace DWIMS.Service.Common;
+
+public interface INotificationService
+{
+    Task SendSubmissionReceivedAsync(
+        Guid submissionId,
+        CancellationToken cancellationToken = default);
+
+    Task SendStepActivatedAsync(
+        Guid submissionId,
+        CancellationToken cancellationToken = default);
+
+    Task SendSubmissionReturnedAsync(
+        Guid submissionId,
+        CancellationToken cancellationToken = default);
+
+    Task SendPasswordResetAsync(
+        Guid userId,
+        string resetToken,
+        CancellationToken cancellationToken = default);
+}
