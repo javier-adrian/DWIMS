a0aae5b4334275738e0e3abf635150441a941408
javier-adrian <agjavier@kld.edu.ph>
2026-04-20 02:05:36 +0800

    add storage models

---
+++ DWIMS.Service/Storage/IStorageService.cs
@@ -0,0 +1,18 @@
+﻿namespace DWIMS.Service.Storage;
+
+public interface IStorageService
+{
+    Task<string> UploadAsync(
+        Stream stream, 
+        string fileName, 
+        string contentType, 
+        CancellationToken cancellationToken = default);
+
+    Task<Stream> DownloadAsync(
+        string storageKey, 
+        CancellationToken cancellationToken = default);
+    
+    Task DeleteAsync(
+        string storageKey, 
+        CancellationToken cancellationToken = default);
+}
---
+++ DWIMS.Service/Storage/StorageOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace DWIMS.Service.Storage;
+
+public sealed class StorageOptions
+{
+    public const string SectionName = "Storage";
+    public required string BucketName { get; init; }
+    public required string AccessKey { get; init; }
+    public required string SecretKey { get; init; }
+    public required string ServiceUrl { get; init; }
+    
+    public string TemplatePrefix { get; init; } = "templates";
+    public string OutputPrefix { get; init; } = "outputs";
+}
