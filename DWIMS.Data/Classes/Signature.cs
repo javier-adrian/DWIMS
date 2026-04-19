15d6cd1a7ec3ea8e8a026f117e3f51a3c08aa157
javier-adrian <agjavier@kld.edu.ph>
2026-04-19 21:50:35 +0800

    store signature on database

--- DWIMS.Data/Classes/Signature.cs
+++ DWIMS.Data/Classes/Signature.cs
@@ -5,5 +5,10 @@ public class Signature
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; }
-    public string Link { get; set; }
+    
+    public required string MimeType { get; set; }
+    public required byte[] EncryptedBlob { get; set; }
+    
+    public DateTime Created { get; set; }
+    public bool isCurrent { get; set; } = true;
 }
--- DWIMS.Data/Migrations/20260419135001_StoreSignatureOnDb.Designer.cs
+++ DWIMS.Data/Migrations/20260419135001_StoreSignatureOnDb.Designer.cs
@@ -4,6 +4,7 @@ using DWIMS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DWIMS.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260419135001_StoreSignatureOnDb")]
+    partial class StoreSignatureOnDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
