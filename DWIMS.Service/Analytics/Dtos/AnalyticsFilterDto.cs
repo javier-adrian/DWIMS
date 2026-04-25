21cd7b84f435ebfc0d11aabad824a2399722e0cc
javier-adrian <agjavier@kld.edu.ph>
2026-04-25 21:45:28 +0800

    add models for analytics service

---
+++ DWIMS.Service/Analytics/Dtos/AnalyticsFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DWIMS.Service.Analytics.Dtos;
+
+public sealed class AnalyticsFilterDto
+{
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
+    public Guid? DepartmentId { get; set; }
+    public Guid? ProcessId { get; set; }
+}
