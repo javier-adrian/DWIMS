b9a4a81b8e24bd16c33e2ea723e658c37a6adb48
javier-adrian <agjavier@kld.edu.ph>
2026-04-26 01:20:05 +0800

    remove redundant data from dto

--- DWIMS.Service/Analytics/Dtos/AnalyticsSummaryDto.cs
+++ DWIMS.Service/Analytics/Dtos/AnalyticsSummaryDto.cs
@@ -5,5 +5,4 @@ public sealed record AnalyticsSummaryDto
     public required ResponseTimeDto ResponseTime { get; init; }
     public required VolumeDto Volume { get; init; }
     public required CycleTimeDto CycleTime { get; init; }
-    public required LeadTimeDto LeadTime { get; init; }
 }
